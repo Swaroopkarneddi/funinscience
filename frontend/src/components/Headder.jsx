7a7df687655d92bb7ece2997cd434a02755d9736
Swaroopkarneddi <swaroopweb07@gmail.com>
2024-09-29 11:35:42 +0530

    'headder'

--- frontend/src/components/Headder.jsx
+++ frontend/src/components/Headder.jsx
@@ -1,7 +1,29 @@
 import React from "react";
 import "./Headder.css";
 function Headder() {
-  return <nav className="headder sticky-top">Headder</nav>;
+  return (
+    <nav className="navbar  headder sticky-top">
+      <h3>Fun In Science.</h3>
+      <input type="checkbox" id="menu-toggle" />
+      <label for="menu-toggle" class="menu-icon">
+        &#9776;
+      </label>
+      <ul className="menu footerlist">
+        <li>
+          <a href="#">Home</a>
+        </li>
+        <li>
+          <a href="#">About</a>
+        </li>
+        <li>
+          <a href="#">Events</a>
+        </li>
+        <li>
+          <a href="#">Contact</a>
+        </li>
+      </ul>
+    </nav>
+  );
 }
 
 export default Headder;
